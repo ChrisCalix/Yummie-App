c38dcc5e78a73b1daf7050e66edec186686b322e
Christian.Starly <yoh.chrisitan@gmail.com>
2023-04-14 17:12:58 -0400

    Create deign for cells in dishes category

---
+++ Yummie/CustomViews/CollectionViewCells/CategoryCollectoinViewCell/CategoryCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Yummie
+//
+//  Created by Christian Calixto on 14/4/23.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryTitleLbl: UILabel!
+
+    func setup(category: DishCategory) {
+        
+    }
+
+}
---
+++ Yummie/Models/DishCategory.swift
@@ -0,0 +1,12 @@
+//
+//  DishCategory.swift
+//  Yummie
+//
+//  Created by Christian Calixto on 14/4/23.
+//
+
+import Foundation
+
+struct DishCategory {
+    let id, name, image: String
+}
